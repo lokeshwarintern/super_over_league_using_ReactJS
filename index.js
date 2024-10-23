50ae77fb3c8c364523c3e7f405c63148e39dffb4
lokeshwarintern <lokeshwaragirishetti1998@gmail.com>
2024-10-23 13:34:33 +0000

    Super Over League tests

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div class="bg-container">
+    <h1 class="heading-ele">Super Over League</h1>
+    <div class="imgs-card">
+      <img
+        class="img-ele"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        class="img-ele"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
